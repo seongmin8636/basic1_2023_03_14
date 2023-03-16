b83a504125d5b5eceea276a1fc531651767f1645
seongmin8636 <moss3004@gmail.com>
2023-03-16 14:28:00 +0900

    loginn

--- src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -4,29 +4,40 @@ import com.ll.basic1.base.rq.Rq;
 import com.ll.basic1.base.rsData.RsData;
 import com.ll.basic1.boundedContext.member.entity.Member;
 import com.ll.basic1.boundedContext.member.service.MemberService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
+@AllArgsConstructor
 public class MemberController {
     private final MemberService memberService;
-
-    // 생성자 주입
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
+    private final Rq rq;
 
     @GetMapping("/member/login")
     @ResponseBody
-    public RsData login(String username, String password, HttpServletRequest req, HttpServletResponse resp) {
-        Rq rq = new Rq(req, resp);
+    public String showLogin() {
+        if (rq.isLogined()) {
+            return """
+                    <h1>이미 로그인 되었습니다.</h1>
+                    """.stripIndent();
+        }
 
+        return """
+                <h1>로그인</h1>
+                <form action="doLogin">
+                <input type="text" placeholder="아이디" name="username">
+                <input type="password" placeholder="비번호" name="password">
+                <input type="submit" value="로그인">
+                </form>
+                """;
+    }
+
+    @GetMapping("/member/doLogin")
+    @ResponseBody
+    public RsData login(String username, String password) {
         if (username == null || username.trim().length() == 0) {
             return RsData.of("F-3", "username(을)를 입력해주세요.");
         }
@@ -39,7 +50,7 @@ public class MemberController {
 
         if (rsData.isSuccess()) {
             Member member = (Member) rsData.getData();
-            rq.setCookie("loginedMemberId", member.getId());
+            rq.setSession("loginedMemberId", member.getId());
         }
 
         return rsData;
@@ -47,9 +58,8 @@ public class MemberController {
 
     @GetMapping("/member/logout")
     @ResponseBody
-    public RsData logout(HttpServletRequest req, HttpServletResponse resp) {
-        Rq rq = new Rq(req, resp);
-        boolean cookieRemoved = rq.removeCookie("loginedMemberId");
+    public RsData logout() {
+        boolean cookieRemoved = rq.removeSession("loginedMemberId");
 
         if (cookieRemoved == false) {
             return RsData.of("S-2", "이미 로그아웃 상태입니다.");
@@ -60,10 +70,8 @@ public class MemberController {
 
     @GetMapping("/member/me")
     @ResponseBody
-    public RsData showMe(HttpServletRequest req, HttpServletResponse resp) {
-        Rq rq = new Rq(req, resp);
-
-        long loginedMemberId = rq.getCookieAsLong("loginedMemberId", 0);
+    public RsData showMe() {
+        long loginedMemberId = rq.getSessionAsLong("loginedMemberId", 0);
 
         boolean isLogined = loginedMemberId > 0;
 
@@ -73,5 +81,12 @@ public class MemberController {
         Member member = memberService.findById(loginedMemberId);
 
         return RsData.of("S-1", "당신의 username(은)는 %s 입니다.".formatted(member.getUsername()));
+    }
+
+    // 디버깅용 함수
+    @GetMapping("/member/session")
+    @ResponseBody
+    public String showSession() {
+        return rq.getSessionDebugContents().replaceAll("\n", "<br>");
     }
 }
